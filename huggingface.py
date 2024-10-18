2b007b0c7030b1e17ac212cce03710397e5033b4
Your Name <luiz.oliveira@ufpr.br>
2024-10-18 15:23:32 -0300

    update huggingface v3

--- huggingface.py
+++ huggingface.py
@@ -1,33 +1,44 @@
-## Transformers to classify the IMDB dataset
+#!/usr/bin/env python3
 
+## Transformers to classify the IMDB dataset
 from transformers import pipeline
 from transformers.pipelines.pt_utils import KeyDataset
 import datasets
 from sklearn.metrics import confusion_matrix
 from tqdm.auto import tqdm
 from sklearn.metrics import classification_report
+from datasets import load_dataset
+from tqdm.auto import tqdm
 
-## loads the dataset 25k samples from the test split
-dataset = datasets.load_dataset("imdb", name="plain_text", split="test")
+## loads the dataset from the csv file
+dataset = load_dataset ('csv', data_files = 'imdb_master-UTF.csv')
 
-## use the model tuned to imdb - device 0 means the GPU
+## selects the 25000 testing samples
+testset = dataset['train'].filter(lambda x: x['type']=='test')
+
 pipe = pipeline(model="textattack/bert-base-uncased-imdb",  device=0)
 
 i =0 
 y_pred =[]
 y = []
 
-for out in pipe(KeyDataset(dataset, "text"), batch_size=32,truncation="only_first"):
-    y.append(dataset['label'][i])
+for out in tqdm(pipe(KeyDataset(testset, "review"), batch_size=32,truncation="only_first")):
+    if (testset['label'][i] == 'neg'):
+        y.append(0)
+    else:
+        y.append(1)
+	
+	
     if (out['label'] == 'LABEL_0' ):
         y_pred.append(0)
     else:
         y_pred.append(1)
-
+		
     i = i+1
     if (i%100==0):
         print(i)
-
+		
 print(confusion_matrix(y, y_pred))
 print(classification_report(y, y_pred))
+
 
